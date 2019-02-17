2028e41cba60623fbe100d7baaacb69261dd548f
Daniel Johansson <johansson.gd@gmail.com>
2019-02-17 16:31:09 +0100

    Initial model migration

---
+++ Library_EF_App/Library_EF_App/Persistence/LibraryContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Library_EF_App.Core.Domain;
+
+namespace Library_EF_App.Persistence
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+        public LibraryContext()
+            : base("name=LibraryContext")
+        {
+
+        }
+    }
+}
