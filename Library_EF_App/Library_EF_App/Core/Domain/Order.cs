7fc2e7c6a5f73ef121fa0bbea22071af86b62c18
Daniel Johansson <johansson.gd@gmail.com>
2019-02-17 14:23:57 +0100

    Skapat domain klasser

---
+++ Library_EF_App/Library_EF_App/Core/Domain/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_EF_App.Core.Domain
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ Library_EF_App/Library_EF_App/Core/Domain/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_EF_App.Core.Domain
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+}
