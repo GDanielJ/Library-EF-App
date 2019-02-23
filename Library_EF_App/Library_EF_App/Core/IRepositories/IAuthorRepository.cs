6e0123fe97cd513076117c857542d7e5b91a2969
Daniel Johansson <johansson.gd@gmail.com>
2019-02-23 18:52:35 +0100

    Skapat IUserRepository och IAuthorRepository.

---
+++ Library_EF_App/Library_EF_App/Core/IRepositories/IAuthorRepository.cs
@@ -0,0 +1,9 @@
+﻿using Library_EF_App.Core.Domain;
+
+namespace Library_EF_App.Core.IRepositories
+{
+    public interface IAuthorRepository : IRepository<Author>
+    {
+        Author GetAuthorWithBooks(int id);
+    }
+}
---
+++ Library_EF_App/Library_EF_App/Core/IRepositories/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using Library_EF_App.Core.Domain;
+
+namespace Library_EF_App.Core.IRepositories
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        User GetUserWithOrders(int id);
+    }
+}
