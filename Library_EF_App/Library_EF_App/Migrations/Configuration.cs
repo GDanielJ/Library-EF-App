b0dc941e4cc85eece14342e8186bc567f3ee8d60
Daniel Johansson <johansson.gd@gmail.com>
2019-02-24 17:22:55 +0100

    Seed-metod för Authors.

--- Library_EF_App/Library_EF_App/Migrations/Configuration.cs
+++ Library_EF_App/Library_EF_App/Migrations/Configuration.cs
@@ -1,9 +1,11 @@
 namespace Library_EF_App.Migrations
 {
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using Library_EF_App.Core.Domain;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Library_EF_App.Persistence.LibraryContext>
     {
@@ -14,18 +16,32 @@ namespace Library_EF_App.Migrations
 
         protected override void Seed(Library_EF_App.Persistence.LibraryContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            var authors = new List<Author>
+            {
+                new Author
+                {
+                    Id = 1,
+                    Firstname = "Selma",
+                    Lastname = "Lagerlöf"
+                },
+                new Author
+                {
+                    Id = 2,
+                    Firstname = "Vilhelm",
+                    Lastname = "Moberg"
+                },
+                new Author
+                {
+                    Id = 3,
+                    Firstname = "Hjalmar",
+                    Lastname = "Söderberg"
+                }
+            };
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            foreach (var author in authors)
+            {
+                context.Authors.AddOrUpdate(a => new { a.Firstname, a.Lastname }, author);
+            }
         }
     }
 }
