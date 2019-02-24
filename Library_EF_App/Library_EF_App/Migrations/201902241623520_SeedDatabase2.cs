472fbc9607b183e917192ed869fb34dd992df463
Daniel Johansson <johansson.gd@gmail.com>
2019-02-24 18:16:49 +0100

    Skapt resten av seed-metoden.

---
+++ Library_EF_App/Library_EF_App/Migrations/201902241623520_SeedDatabase2.cs
@@ -0,0 +1,16 @@
+namespace Library_EF_App.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SeedDatabase2 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
