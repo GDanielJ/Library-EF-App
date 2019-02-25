b23242111c1e06ffbcbd1200e8c0ab8cca62d1df
Daniel Johansson <johansson.gd@gmail.com>
2019-02-25 20:15:32 +0100

    Skapat AuthorRepository.

---
+++ Library_EF_App/Library_EF_App/Migrations/201902251900108_ReSeed.cs
@@ -0,0 +1,16 @@
+namespace Library_EF_App.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ReSeed : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ Library_EF_App/Library_EF_App/Persistence/Repositories/AuthorRepository.cs
@@ -0,0 +1,24 @@
+﻿using Library_EF_App.Core.IRepositories;
+using Library_EF_App.Core.Domain;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Library_EF_App.Persistence.Repositories
+{
+    public class AuthorRepository : Repository<Author>, IAuthorRepository
+    {
+        public AuthorRepository(LibraryContext context) : base(context) // Vad gör sista delen här? ( : base(context)). base låter mig få tillgång till constructorn i DbContext, istället för klassen Repository? Varför gör jag såhär? :)
+        {
+        }
+
+        public Author GetAuthorWithBooks(int id)
+        {
+            return LibraryContext.Authors.Include(a => a.Books).SingleOrDefault(a => a.Id == id);
+        }
+
+        public LibraryContext LibraryContext // Vad är poängen med att göra så här?
+        {
+            get { return Context as LibraryContext; }
+        }
+    }
+}
