005a7fa1381cd1c3537f2ad24e5cf76af972915b
Daniel Johansson <johansson.gd@gmail.com>
2019-03-02 19:32:35 +0100

    Skapat IOrderRepository och OrderRepository.

---
+++ Library_EF_App/Library_EF_App/Core/IRepositories/IBookRepository.cs
@@ -0,0 +1,14 @@
+﻿using Library_EF_App.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_EF_App.Core.IRepositories
+{
+    public interface IBookRepository : IRepository<Book>
+    {
+
+    }
+}
---
+++ Library_EF_App/Library_EF_App/Persistence/EntityConfigurations/OrderRepository.cs
@@ -0,0 +1,25 @@
+﻿using Library_EF_App.Persistence.Repositories;
+using Library_EF_App.Core.Domain;
+using Library_EF_App.Core.IRepositories;
+using System.Linq;
+using System.Data.Entity;
+
+namespace Library_EF_App.Persistence.EntityConfigurations
+{
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+        public OrderRepository(LibraryContext context) : base(context)
+        {
+        }
+
+        public Order GetOrderWithUser(int id)
+        {
+            return LibraryContext.Orders.Include(o => o.User).SingleOrDefault(o => o.Id == id);
+        }
+
+        public LibraryContext LibraryContext
+        {
+            get { return Context as LibraryContext; }
+        }
+    }
+}
