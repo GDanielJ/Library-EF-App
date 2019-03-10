0e3c2a205a443d36d9d4b42bf6eb5174f6517b83
Daniel Johansson <johansson.gd@gmail.com>
2019-03-10 19:12:46 +0100

    Jobbat mer med logiken i UI:n.

--- Library_EF_App/Library_EF_App/Program.cs
+++ Library_EF_App/Library_EF_App/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Library_EF_App.Persistence;
+using Library_EF_App.Persistence.Repositories;
 
 namespace Library_EF_App
 {
@@ -23,48 +25,63 @@ namespace Library_EF_App
     // Tänkte lösa det här med att använda mig av en submeny (DisplaySubmenu()) och sen försöka göra en generell switch/case,
     // men vet inte hur jag ska lösa det.
 
+
+    // Uppdatering: Kanske kommit på ett sätt att lösa ovan. Se början på metod nedan som heter subFind, där jag tänker mig skapa
+    // lämplig repository baserat på vilket val man gör i första menyn (dvs vad värdet på "key" är). Skapar nya problem dock.
+
     public class Program
     {
         public static void Main(string[] args)
         {
-            string key;
-
-            do
+            using (var unitOfWork = new UnitOfWork(new LibraryContext()))
             {
-                Console.Clear();
-                DisplayMenu();
-                key = Console.ReadLine();
+                string key;
 
-                string key2;
                 do
                 {
                     Console.Clear();
-                    DisplaySubmenu(key);
-                    key2 = Console.ReadLine();
+                    DisplayMenu();
+                    key = Console.ReadLine();
 
-                    switch (key2)
+                    string key2;
+                    do
                     {
-                        case "1":
-                            // Metod
-                            break;
-                        case "2":
-                            // Metod
-                            break;
-                        case "3":
-                            // Metod
-                            break;
-                        case "4":
-                            // Metod
-                            break;
-                        default:
-                            Console.WriteLine("Invalid command");
-                            Console.ReadLine();
-                            break;
-                    }
-                } while (key2 != "9");
+                        Console.Clear();
+                        DisplaySubmenu(key);
+                        key2 = Console.ReadLine();
 
-            } while (key != "9");
+                        switch (key2)
+                        {
+                            case "1":
+                                // Metod
+                                break;
+                            case "2":
+                                // Metod
+                                break;
+                            case "3":
+                                // Metod
+                                break;
+                            case "4":
+                                // Metod
+                                break;
+                            default:
+                                Console.WriteLine("Invalid command");
+                                Console.ReadLine();
+                                break;
+                        }
+                    } while (key2 != "9");
+
+                } while (key != "9");
+            }
         }
+
+        public static Dictionary<int, string> choices = new Dictionary<int, string>()
+        {
+            {1, "user"},
+            {2, "loan"},
+            {3, "author"},
+            {4, "book"}
+        };
 
         public static void DisplayMenu()
         {
@@ -78,14 +95,6 @@ namespace Library_EF_App
             Console.WriteLine("\t9. Exit");
         }
 
-        public static Dictionary<int, string> choices = new Dictionary<int, string>()
-        {
-            {1, "user"},
-            {2, "loan"},
-            {3, "author"},
-            {4, "book"}
-        };
-
         public static void DisplaySubmenu(string key)
         {
             Console.WriteLine($"Manage {choices[Convert.ToInt32(key)]}s");
@@ -95,6 +104,20 @@ namespace Library_EF_App
             Console.WriteLine($"\t3. Update {choices[Convert.ToInt32(key)]}");
             Console.WriteLine($"\t4. Remove {choices[Convert.ToInt32(key)]}");
             Console.WriteLine("\t9. Exit");
+        }
+
+        public static void subFind(string key, UnitOfWork unitOfWork)
+        {
+            // User
+            if (key == "1")
+                var repository = new UserRepository(unitOfWork); // Ska mina repositories ta en UnitOfWork i sin konstruktor istället för en LibraryContext?
+            // Loan
+            if (key == "2")
+                var repository = new OrderRepository(unitOfWork);
+            // Author
+
+            // Book
+
         }
 
 
